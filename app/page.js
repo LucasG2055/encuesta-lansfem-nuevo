2c498d7faec7be8714a941b6aa46ed6fab8a6da6
LucasG2055 <88249772+LucasG2055@users.noreply.github.com>
2025-03-04 22:03:46 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontSize: '48px',
+      fontWeight: 'bold'
+    }}>
+      Hola
+    </div>
+  )
+}
