8489891bcae3a3e0ff8e0f94ec5644f15ee22f23
LucasG2055 <88249772+LucasG2055@users.noreply.github.com>
2025-03-04 23:11:45 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import Image from 'next/image';
-
+import { useRouter } from 'next/navigation';
 // Componente de calificación con estrellas
 function StarRating({ rating, onRatingChange }) {
   const [hoverRating, setHoverRating] = useState(0);
