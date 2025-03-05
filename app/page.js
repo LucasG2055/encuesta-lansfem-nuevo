a9708d02c92cafa17cd35a12270f598219b3ff8d
LucasG2055 <88249772+LucasG2055@users.noreply.github.com>
2025-03-04 22:25:36 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import SimpleForm from '../components/SimpleForm';
+import SimpleForm from '@/components/SimpleForm'; // Cambiado de '../components/SimpleForm'
 
 export default function Home() {
   return (
